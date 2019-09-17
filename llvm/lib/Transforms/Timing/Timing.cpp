7593609d8456723eabdab0eed8206fb9f062113c
Vito Alexander Nordloh <vito.nordloh@vitonordloh.de>
2019-09-17 12:03:46 +0200

    Improve timing method

--- llvm/lib/Transforms/Timing/Timing.cpp
+++ llvm/lib/Transforms/Timing/Timing.cpp
@@ -14,22 +14,39 @@ double cps = CLOCKS_PER_SEC;
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/APInt.h"
 #include "llvm/Support/CommandLine.h"
 
+#include <vector>
+#include <time.h>
+
 using namespace std;
 using namespace llvm;
+
+clockid_t clock_id = CLOCK_MONOTONIC;
 
 struct Timing : public ModulePass {
     static char ID;
     Timing() : ModulePass(ID) {}
 
     bool runOnModule(Module &M) override {
+        // Create timespec struct
+        vector<Type*> elements;
+        elements.push_back(Type::getInt32Ty(M.getContext()));
+        elements.push_back(Type::getInt64Ty(M.getContext()));
+        StructType* timespec = StructType::create(M.getContext(), elements);
+
         // Create clock function
-        Type* clock_t = Type::getInt64Ty(M.getContext());
-        FunctionType* clockTy = FunctionType::get(clock_t, false);
-        Value* clock = M.getOrInsertFunction("clock", clockTy);
+        Type* clock_t = Type::getInt32Ty(M.getContext());
+
+        vector<Type*> clockParams;
+        clockParams.push_back(Type::getInt32Ty(M.getContext()));
+        clockParams.push_back(timespec->getPointerTo());
+
+        FunctionType* clockTy = FunctionType::get(clock_t, clockParams, false);
+        Value* clock = M.getOrInsertFunction("clock_gettime", clockTy);
 
         // Create printf function
         Type* printf_t = Type::getVoidTy(M.getContext());
@@ -41,11 +58,18 @@ struct Timing : public ModulePass {
         for(auto& F : M) {
             if(F.getName() == "main") {
                 IRBuilder<> builder(M.getContext());
-
-                // Insert first call to clock
                 Instruction* firstInstruction = F.getEntryBlock().getFirstNonPHIOrDbg();
                 builder.SetInsertPoint(firstInstruction);
-                CallInst* start = builder.CreateCall(clock);
+
+                // Create structs for start and end
+                AllocaInst* allocStart = builder.CreateAlloca(timespec);
+                AllocaInst* allocEnd = builder.CreateAlloca(timespec);
+
+                // Insert first call to clock_gettime
+                vector<Value*> paramsFirstCall;
+                paramsFirstCall.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()), clock_id));
+                paramsFirstCall.push_back(allocStart);
+                CallInst* start = builder.CreateCall(clock, paramsFirstCall);
 
                 for(auto& BB : F) {
                     for(auto& I : BB) {
@@ -65,18 +89,40 @@ struct Timing : public ModulePass {
                         // Insert call
                         if(insert) {
                             builder.SetInsertPoint(&I);
-                            CallInst *end = builder.CreateCall(clock);
+
+                            // Insert second call
+                            vector<Value*> paramsSecondCall;
+                            paramsSecondCall.push_back(ConstantInt::get(Type::getInt32Ty(M.getContext()), clock_id));
+                            paramsSecondCall.push_back(allocEnd);
+                            CallInst* end = builder.CreateCall(clock, paramsSecondCall);
 
                             // Calculate time
-                            Value *diff = builder.CreateSub(end, start);
-                            Value *castedDiff = builder.CreateUIToFP(diff, Type::getDoubleTy(M.getContext()));
-                            Constant *clocksPerSec = ConstantFP::get(Type::getDoubleTy(M.getContext()), cps);
-                            Value *div = builder.CreateFDiv(castedDiff, clocksPerSec);
+                            Value *startPtrS = builder.CreateStructGEP(timespec, allocStart, 0);
+                            Value *startTimeS = builder.CreateLoad(startPtrS);
+                            Value *startPtrNS = builder.CreateStructGEP(timespec, allocStart, 1);
+                            Value *startTimeNS = builder.CreateLoad(startPtrNS);
+
+                            Value *endPtrS = builder.CreateStructGEP(timespec, allocEnd, 0);
+                            Value *endTimeS = builder.CreateLoad(endPtrS);
+                            Value *endPtrNS = builder.CreateStructGEP(timespec, allocEnd, 1);
+                            Value *endTimeNS = builder.CreateLoad(endPtrNS);
+
+                            Value *diffS = builder.CreateSub(endTimeS, startTimeS);
+                            Value *diffNS = builder.CreateSub(endTimeNS, startTimeNS);
+
+                            Value *castedDiffS = builder.CreateUIToFP(diffS, Type::getDoubleTy(M.getContext()));
+                            Value *castedDiffNS = builder.CreateUIToFP(diffNS, Type::getDoubleTy(M.getContext()));
+
+                            Constant *nsPerSec = ConstantFP::get(Type::getDoubleTy(M.getContext()), 1E9);
+                            Value *div = builder.CreateFDiv(castedDiffNS, nsPerSec);
+
+                            Value *final = builder.CreateFAdd(castedDiffS, div);
 
                             // Print time
-                            Constant *format = builder.CreateGlobalStringPtr("Total Time: %f");
-                            vector < Value * > p;
+                            Constant *format = builder.CreateGlobalStringPtr("Total Time: %fs + %fns");
+                            vector<Value*> p;
                             p.push_back(format);
+                            p.push_back(castedDiffS);
                             p.push_back(div);
                             builder.CreateCall(printf, p);
                         }
