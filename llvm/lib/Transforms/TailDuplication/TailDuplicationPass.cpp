2458988856b76bda5d29fab81ca042c3a8ac8b71
Vito Alexander Nordloh <vito.nordloh@vitonordloh.de>
2019-10-20 11:22:38 +0200

    Update

--- llvm/lib/Transforms/TailDuplication/TailDuplicationPass.cpp
+++ llvm/lib/Transforms/TailDuplication/TailDuplicationPass.cpp
@@ -33,7 +33,7 @@ namespace {
     };
 
     bool TailDuplication::runOnFunction(Function &F) {
-        if(!F.getName().equals("std_eval")) {
+        if(!F.getName().equals("FastFullPelBlockMotionSearch")) {
             return false;
         }
         this->F = &F;
